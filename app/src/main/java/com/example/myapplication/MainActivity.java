2f416f68d5b5c4d7c073521e780b75dad8ce7349
Nhat Lam <lamdeptrai1796@gmail.com>
2019-05-27 08:54:31 +0700

    baucua

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonPlay;
+    ImageView imageView3,imageView4,imageView5;
+    ArrayList<Integer> mang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonPlay = (Button)findViewById(R.id.buttonPlay);
+        imageView3 = (ImageView) findViewById(R.id.imageView3);
+        imageView4 = (ImageView) findViewById(R.id.imageView4);
+        imageView5 = (ImageView) findViewById(R.id.imageView5);
+
+        mang = new ArrayList<Integer>();
+        mang.add(R.drawable.bau);
+        mang.add(R.drawable.cua);
+        mang.add(R.drawable.tom);
+        mang.add(R.drawable.ca);
+        mang.add(R.drawable.nai);
+        mang.add(R.drawable.ga);
+
+        buttonPlay. setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+
+
+
+                Random r = new Random();
+                 int n = r.nextInt(mang.size());
+                imageView3.setImageResource(mang.get(n));
+
+                int m = r.nextInt(mang.size());
+                imageView4.setImageResource(mang.get(m));
+
+                int k = r.nextInt(mang.size());
+                imageView5.setImageResource(mang.get(k));
+
+            }
+        });
+
+    }
+}
